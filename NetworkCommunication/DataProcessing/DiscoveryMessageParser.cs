487b40d572f69b9ca242b7cf47fb5cf70a1497b9
Jan Scholtyssek <jan@janscholtyssek.dk>
2018-10-21 23:38:38 +0200

    Filtering patient info in discovery message for non-printable characters. Replacing missing name with "Unknown Name"

--- NetworkCommunication/DataProcessing/DiscoveryMessageParser.cs
+++ NetworkCommunication/DataProcessing/DiscoveryMessageParser.cs
@@ -17,22 +17,30 @@ namespace NetworkCommunication.DataProcessing
                 .Skip(12)
                 .TakeWhile(b => b != WardBedSeparator)
                 .Select(b => (char)b)
+                .Where(IsPrintableCharacter)
                 .ToArray());
             var bedName = new string(data
                 .Skip(12+wardName.Length+1)
                 .TakeWhile(b => b != 0x00)
                 .Select(b => (char)b)
+                .Where(IsPrintableCharacter)
                 .ToArray());
             var lastName = new string(data
                 .Skip(28)
                 .Select(b => (char)b)
                 .TakeWhile(c => c != ',')
+                .Where(IsPrintableCharacter)
                 .ToArray());
+            if (string.IsNullOrWhiteSpace(lastName))
+                lastName = "Name";
             var firstName = new string(data
                 .Skip(28 + lastName.Length + 1)
                 .TakeWhile(b => b != 0x00)
                 .Select(b => (char) b)
+                .Where(IsPrintableCharacter)
                 .ToArray());
+            if (string.IsNullOrWhiteSpace(firstName))
+                firstName = "Unknown";
             var patientInfo = new PatientInfo(firstName, lastName);
             return new DiscoveryMessage(
                 ipAddress,
@@ -41,6 +49,11 @@ namespace NetworkCommunication.DataProcessing
                 bedName, 
                 patientInfo, 
                 timestamp);
+        }
+
+        private bool IsPrintableCharacter(char c)
+        {
+            return char.IsLetter(c) || c.InSet('-', '.', ' ');
         }
     }
 }
